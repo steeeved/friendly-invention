718a38dfc2e25c01647a341a14ac0d9053fe82d1
Stephen Muchendu <muchendumaina66@gmail.com>
2022-09-21 12:50:27 +0300

    updates

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -3,11 +3,14 @@ import { signInWithPopup } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 import { ILogged } from '../../Elements/types';
 
+import Styles from './Login.module.scss';
+import GoogleIcon from '@mui/icons-material/Google';
+
 const Login = (props: ILogged) => {
   const { setLogged } = props;
   const navigate = useNavigate();
   setLogged(false);
-  
+
   const signInWithGoogle = async () => {
     const result = await signInWithPopup(auth, provider);
     navigate('/');
@@ -15,10 +18,14 @@ const Login = (props: ILogged) => {
   };
 
   return (
-    <div>
-      <h1>This is the login PAGE</h1>
-      <p>Sign in with Google</p>
-      <button onClick={signInWithGoogle}>Sign in With Google</button>
+    <div className={Styles.main}>
+      <div className={Styles.google}>
+        <button onClick={signInWithGoogle}>Sign in With Google</button>
+      </div>
+      <div className={Styles.name}>
+        <p>Use your name instead</p>
+        <input placeholder="Enter your name" />
+        </div>
     </div>
   );
 };
