d0e10ad68c2bee50e14f0b3988e01db40fc64380
Stephen Muchendu <muchendumaina66@gmail.com>
2022-09-21 07:11:06 +0300

    starter

---
+++ src/Pages/index.ts
@@ -0,0 +1,4 @@
+export {default as MainPage} from './Main/MainPage';
+export { default as Error } from './404/Error';
+export { default as Login } from './Login/Login';
+export {default as CreatePost} from './CreatePost/CreatePost';
